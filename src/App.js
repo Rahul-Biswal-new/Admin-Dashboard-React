740f45c7278f0e98eb37ade883e2ac0e9fd0e7ae
Rahul <rahulbiswalnew@gmail.com>
2023-01-23 01:59:04 +0530

    second added Topbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-
+import { ColorModeContext, useMode } from "./Theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import Topbar from "./scenes/global/Topbar"
 
 function App() {
-  return (
+  const [theme , colorMode] = useMode();
+
+  return ( <ColorModeContext.Provider value={colorMode}>
+    <ThemeProvider theme={theme}>
+      <CssBaseline/>
     <div className="App">
-        <h1>Hello World</h1>
+        <main className="content">
+          <Topbar></Topbar>
+        </main>
     </div>
+    </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
