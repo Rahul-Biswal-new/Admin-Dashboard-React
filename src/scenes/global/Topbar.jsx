b543abe5ca5ac48d72c0b8f0a40941ce926f9793
Rahul <rahulbiswalnew@gmail.com>
2023-01-31 19:04:40 +0530

    Fourth commit topbar route added

--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -9,7 +9,7 @@ import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined
 import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
 import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
 import SearchIcon from "@mui/icons-material/Search";
-import styled from "@emotion/styled";
+// import styled from "@emotion/styled";
 
 
 
