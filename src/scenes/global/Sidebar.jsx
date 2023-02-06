f90199bab6371e6083d1712fa2d333d9e793f323
Rahul <rahulbiswalnew@gmail.com>
2023-02-06 22:46:02 +0530

    sixth sidebar  commit

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -1,15 +1,40 @@
 import React from 'react'
 import { useState } from 'react';
+import { ProSidebar, Menu, MenuItem } from 'react-pro-sidebar';
+import {Box, IconButton, Typography, useTheme} from '@mui/material';
+import {Link} from 'react-router-dom';
+import {tokens } from '../../Theme';
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import PeopleOutlinedIcon from "@mui/icons-material/PeopleOutlined";
+import ContactsOutlinedIcon from "@mui/icons-material/ContactsOutlined";
+import ReceiptOutlinedIcon from "@mui/icons-material/ReceiptOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import CalendarTodayOutlinedIcon from "@mui/icons-material/CalendarTodayOutlined";
+import HelpOutlinedIcon from "@mui/icons-material/HelpOutlined";
+import BarChartOutlinedIcon from "@mui/icons-material/BarChartOutlined";
+import PieChartOutlineOutlinedIcon from "@mui/icons-material/PieChartOutlineOutlined";
+import TimelineOutlinedIcon from "@mui/icons-material/TimelineOutlined";
+import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+
 
 const Sidebar =()=> {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const [isCollapsed, setisCollapsed] = useState(false);
+  const [selected, setselected] = useState("Dashboard");
 
   return (
-    <div>Sidebar</div>
+    <Box>
+      sx={{
+        
+      }}
+    </Box>
   )
 }
 
 
-
+  
 
 
 export default Sidebar;
